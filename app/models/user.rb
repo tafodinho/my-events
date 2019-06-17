329787d5fde669ac3f4263adaa3fcb65ee03967f
Tafang Joshua <tafodinho@gmail.com>
2019-06-17 22:12:54 +0100

    fixes to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 
     validates :name, presence: true
-    validates :email, presence: true, unique: true
+    validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: { minimum: 6 }
 
     has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "users#new"
   get '/login', to: 'sessions#new'
   get '/signup', to: 'users#new'
-  
+  post '/signup', to: 'users#create'
+
   resources :users
 end
