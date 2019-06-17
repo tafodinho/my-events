0de6930bf49a5650196856ffe4bd7e5feee2e6b6
Tafang Joshua <tafodinho@gmail.com>
2019-06-17 21:49:50 +0100

    added signup functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :password, presence: true, length: { minimum: 6 }
+
+    has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'user/new,'
-  get 'user/create,'
-  get 'user/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+  get 'sessions/new'
+  get 'sessions/create'
+  root "users#new"
+  resources :users
 end
