33974841bae3a76b59cf1de22eb517e2314865cc
Tafang Joshua <tafodinho@gmail.com>
2019-06-21 18:13:48 +0100

    modified model methods 'pas' and 'upcoming' to scopes in the event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,9 @@
 class Event < ApplicationRecord
+  scope :past, lambda { where("date < ?", Time.zone.now) }
+  scope :upcoming, lambda { where("date >= ?", Time.zone.now) }
+
   belongs_to :creator, foreign_key: "creator_id", class_name: "User"
-  has_many :user_events, class_name: "UserEvent", foreign_key: "attendee_id"
+  has_many :user_events, class_name: "UserEvent", foreign_key: "attended_event_id"
    
   has_many :attendees, through: :user_events, source: :attendee
 
@@ -8,11 +11,5 @@ class Event < ApplicationRecord
   validates :venue, presence: true
   validates :date, presence: true
 
-  def past
-    return date if date < Time.zone.now
-  end
-
-  def upcoming 
-    return date if date > Time.zone.now
-  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :events, foreign_key: "creator_id", class_name: "Event"
-    has_many :user_events, class_name: "UserEvent",foreign_key: "attended_event_id"
+    has_many :user_events, class_name: "UserEvent", foreign_key: "attendee_id"
     has_many :attended_events, through: :user_events, source: :attended_event
     
     validates :name, presence: true
