99f461f8f169b2edb97fb2f9ac0ce9ba11d36607
offorma <offorma@gmail.com>
2019-06-22 15:39:29 +0100

    user logs in on create and password removed from signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
     
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
-    validates :password, presence: true, length: { minimum: 6 }
 
-    has_secure_password
 
     def previous_events
       attended_events.where('date < :current_date',
