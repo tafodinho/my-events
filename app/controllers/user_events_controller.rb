172b685423e9c2bda8e3bb69039363681348752d
offorma <offorma@gmail.com>
2019-06-22 16:01:06 +0100

    added validation to ensure user cant create multiple attendance

--- app/controllers/user_events_controller.rb
+++ app/controllers/user_events_controller.rb
@@ -2,12 +2,17 @@ class UserEventsController < ApplicationController
     def create 
         event_id = params[:id]
         user_event = UserEvent.new(attendee_id: current_user.id, attended_event_id: event_id)
-        puts current_user.id
-        if user_event.save 
-            flash[:success] = "Successfully subscribed"
-            redirect_to events_path
+        existing_user_event = UserEvent.find_by(attendee_id: current_user.id, attended_event_id: event_id)
+        if existing_user_event == nil
+            if user_event.save 
+                flash[:success] = "Successfully subscribed"
+                redirect_to events_path
+            else
+                flash[:danger] = "Failed to subscribe"
+                redirect_to events_path
+            end
         else
-            flash[:danger] = "Failed to subscribe"
+            flash[:success] = "You have already subscribed to event"
             redirect_to events_path
         end
     end
